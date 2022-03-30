3618627dc67b487811cce7cdda54a2ffc6758cfd
hacadena28 <100697841+hacadena28@users.noreply.github.com>
2022-03-30 10:10:07 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+/*Aplicacion que me guarde los datos en una tabla*/
+var nombre;
+var apellido;
+var telefono;
+var email;
+var direccion;
+var fecha;
+var nota;
+var tabla = document.getElementById("tabla");
+var boton = document.getElementById("guardar");
+var contador = 1;
+console.log("Javascript funcionando");
+function adicionarATabla(){
+   
+}
+boton.addEventListener("click",function(){
+    console.log("Se ha dado click al boton");
+})
+
